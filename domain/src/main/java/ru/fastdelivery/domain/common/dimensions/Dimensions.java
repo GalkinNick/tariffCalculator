e2265a0acfb9045dbfd1d506adb38c982126983e
User <nicolas_galkin@mail.ru>
2025-08-20 22:52:24 +0500

    Проверка веса, вес не может быть меньше 0

--- domain/src/main/java/ru/fastdelivery/domain/common/dimensions/Dimensions.java
+++ domain/src/main/java/ru/fastdelivery/domain/common/dimensions/Dimensions.java
@@ -5,8 +5,26 @@ import java.math.BigInteger;
 public record Dimensions(
         BigInteger length,
         BigInteger width,
-        BigInteger height
-                         ) {
+        BigInteger height) implements Comparable<Dimensions>{
+
+    private static boolean isLassThenZero(BigInteger price){return BigInteger.ZERO.compareTo(price) > 0;}
 
 
+    public Dimensions{
+        if (isLassThenZero(length)){
+            throw new IllegalArgumentException("Weight cannot be below Zero!");
+        }
+        if (isLassThenZero(width)){
+            throw new IllegalArgumentException("Weight cannot be below Zero!");
+        }
+        if (isLassThenZero(height )){
+            throw new IllegalArgumentException("Weight cannot be below Zero!");
+        }
+    }
+
+
+    @Override
+    public int compareTo(Dimensions d) {
+        return d.length().compareTo(length()) + d.width().compareTo(width()) + d.height().compareTo(height());
+    }
 }
